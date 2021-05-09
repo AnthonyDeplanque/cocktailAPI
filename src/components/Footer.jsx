23099db3cd0add9ccfd876fbd2fe3cc83fdfbd60
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-05-09 17:14:23 +0200

    styled box for cocktail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,16 @@ import "./Footer.css";
 
 export default function Footer(){
     return <div className="footer">
-        <p className="legal">
+        <div className="icons"></div>
+        <div className="legal"><p>
             This website is intended for adults
             over 21 and of legal drinking age.
             We encourage responsible drinking only.
             Nothing on this website should be construed
             as professional advice. All content on this
             website is for informational and educational
-            purposes only.
-        </p>
+            purposes only.</p>
+        </div>
+        <div className="copyright"></div>
     </div>
 }
--- src/components/GetSelectedCocktail.jsx
+++ src/components/GetSelectedCocktail.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import API_ID from "../constants/API_ID";
-import "./GetSelectedCocktail.css"
+import "./GetSelectedCocktail.css";
 
 function GetSelectedCocktail(viewCocktail) {
   const [viewedCocktail, setViewedCocktail] = useState({});
@@ -48,46 +48,119 @@ function GetSelectedCocktail(viewCocktail) {
     strMeasure9,
   } = viewedCocktail;
 
-useEffect(() => {
- axios
-  .get(`${API_ID}${viewCocktail.cocktailId}`)
-  .then(r=> r.data)
-  .then(r=> r.drinks)
-  .then(r=>r)
-  .then(r=>setViewedCocktail(r[0]));
-  console.log(viewedCocktail);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    axios
+      .get(`${API_ID}${viewCocktail.cocktailId}`)
+      .then((r) => r.data)
+      .then((r) => r.drinks)
+      .then((r) => r)
+      .then((r) => setViewedCocktail(r[0]));
+    console.log(viewedCocktail);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [viewCocktail.cocktailId, idDrink]);
 
-  if (idDrink){ return (
-    <div className="cocktail-box">
-      <h1>{strDrink}</h1>
-      {strDrinkAlternate && <p><em>{strDrinkAlternate}</em></p>}
-      <h3>{strAlcoholic}</h3>
-      <img className="cocktail-image" src={strDrinkThumb} alt={idDrink}/>
-      <p>serve in a <strong>{strGlass}</strong></p>
-      <div className="ingredients-list">
-        <ul>
-        {strIngredient && <li className="ingredient">{strMeasure1} <strong>{strIngredient}</strong></li>}
-        {strIngredient2 && <li className="ingredient">{strMeasure2} <strong>{strIngredient2}</strong></li>}
-        {strIngredient3 && <li className="ingredient">{strMeasure3} <strong>{strIngredient3}</strong></li>}
-        {strIngredient4 && <li className="ingredient">{strMeasure4} <strong>{strIngredient4}</strong></li>}
-        {strIngredient5 && <li className="ingredient">{strMeasure5} <strong>{strIngredient5}</strong></li>}
-        {strIngredient6 && <li className="ingredient">{strMeasure6} <strong>{strIngredient6}</strong></li>}
-        {strIngredient7 && <li className="ingredient">{strMeasure7} <strong>{strIngredient7}</strong></li>}
-        {strIngredient8 && <li className="ingredient">{strMeasure8} <strong>{strIngredient8}</strong></li>}
-        {strIngredient9 && <li className="ingredient">{strMeasure9} <strong>{strIngredient9}</strong></li>}
-        {strIngredient10 && <li className="ingredient">{strMeasure10} <strong>{strIngredient10}</strong></li>}
-        {strIngredient11 && <li className="ingredient">{strMeasure11} <strong>{strIngredient11}</strong></li>}
-        {strIngredient12 && <li className="ingredient">{strMeasure12} <strong>{strIngredient12}</strong></li>}
-        {strIngredient13 && <li className="ingredient">{strMeasure13} <strong>{strIngredient13}</strong></li>}
-        {strIngredient14 && <li className="ingredient">{strMeasure14} <strong>{strIngredient14}</strong></li>}
-        {strIngredient15 && <li className="ingredient">{strMeasure15} <strong>{strIngredient15}</strong></li>}
-        </ul>
-        </div>  
-        {strInstructions && <p>{strInstructions}</p>}
-    </div>
-  ); }
-  else return null;
+  if (idDrink) {
+    return (
+      <div className="cocktail-box">
+        <h1>{strDrink}</h1>
+        {strDrinkAlternate && (
+          <p>
+            <em>{strDrinkAlternate}</em>
+          </p>
+        )}
+        <div className="cocktail-elements">
+          <div className="cocktail-intro">
+            <h3>{strAlcoholic}</h3>
+            <img className="cocktail-image" src={strDrinkThumb} alt={idDrink} />
+            <p>
+              serve in a <strong>{strGlass}</strong>
+            </p>
+          </div>
+          <div className="cocktail-making">
+            <div className="ingredients-list">
+              <ul>
+                {strIngredient && (
+                  <li className="ingredient">
+                    {strMeasure1} <strong>{strIngredient}</strong>
+                  </li>
+                )}
+                {strIngredient2 && (
+                  <li className="ingredient">
+                    {strMeasure2} <strong>{strIngredient2}</strong>
+                  </li>
+                )}
+                {strIngredient3 && (
+                  <li className="ingredient">
+                    {strMeasure3} <strong>{strIngredient3}</strong>
+                  </li>
+                )}
+                {strIngredient4 && (
+                  <li className="ingredient">
+                    {strMeasure4} <strong>{strIngredient4}</strong>
+                  </li>
+                )}
+                {strIngredient5 && (
+                  <li className="ingredient">
+                    {strMeasure5} <strong>{strIngredient5}</strong>
+                  </li>
+                )}
+                {strIngredient6 && (
+                  <li className="ingredient">
+                    {strMeasure6} <strong>{strIngredient6}</strong>
+                  </li>
+                )}
+                {strIngredient7 && (
+                  <li className="ingredient">
+                    {strMeasure7} <strong>{strIngredient7}</strong>
+                  </li>
+                )}
+                {strIngredient8 && (
+                  <li className="ingredient">
+                    {strMeasure8} <strong>{strIngredient8}</strong>
+                  </li>
+                )}
+                {strIngredient9 && (
+                  <li className="ingredient">
+                    {strMeasure9} <strong>{strIngredient9}</strong>
+                  </li>
+                )}
+                {strIngredient10 && (
+                  <li className="ingredient">
+                    {strMeasure10} <strong>{strIngredient10}</strong>
+                  </li>
+                )}
+                {strIngredient11 && (
+                  <li className="ingredient">
+                    {strMeasure11} <strong>{strIngredient11}</strong>
+                  </li>
+                )}
+                {strIngredient12 && (
+                  <li className="ingredient">
+                    {strMeasure12} <strong>{strIngredient12}</strong>
+                  </li>
+                )}
+                {strIngredient13 && (
+                  <li className="ingredient">
+                    {strMeasure13} <strong>{strIngredient13}</strong>
+                  </li>
+                )}
+                {strIngredient14 && (
+                  <li className="ingredient">
+                    {strMeasure14} <strong>{strIngredient14}</strong>
+                  </li>
+                )}
+                {strIngredient15 && (
+                  <li className="ingredient">
+                    {strMeasure15} <strong>{strIngredient15}</strong>
+                  </li>
+                )}
+              </ul>
+            </div>
+            {strInstructions && <p>{strInstructions}</p>}
+          </div>
+        </div>
+      </div>
+    );
+  } else return null;
 }
 export default GetSelectedCocktail;
