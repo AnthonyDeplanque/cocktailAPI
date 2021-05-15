ae6726befe7b45537160a349d5cec4271a294cc1
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-05-15 19:51:16 +0200

    added burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,46 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { useEffect, useState } from 'react';
+import './BurgerMenu.css';
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  useEffect(() => {
+    console.log(showMenu);
+  }, [showMenu]);
+
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li>
-          <Link to="/">Filtered Cocktails</Link>
-        </li>
-        <li>
-          <Link to="/RandomCocktail">Random Cocktail</Link>
-        </li>
-        <li>
-          <Link to="/SearchCocktail">Search a Cocktail</Link>
-        </li>
-        <div className="navbar-logo">
-          <img src="https://www.thecocktaildb.com/images/cocktail_right.png" alt="the cocktail dispenser" />
-          <h1>The Cocktail Dispenser !</h1>
-        </div>
-      </ul>
+      <div
+        aria-hidden="true"
+        className="burger-menu"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <div className="line line-1" />
+        <div className="line line-2" />
+        <div className="line line-3" />
+      </div>
+
+      {showMenu && (
+        <ul className="navbar-list">
+          <li>
+            <Link to="/">Filtered Cocktails</Link>
+          </li>
+          <li>
+            <Link to="/RandomCocktail">Random Cocktail</Link>
+          </li>
+          <li>
+            <Link to="/SearchCocktail">Search a Cocktail</Link>
+          </li>
+          <div className="navbar-logo">
+            <img
+              src="https://www.thecocktaildb.com/images/cocktail_right.png"
+              alt="the cocktail dispenser"
+            />
+            <h1>The Cocktail Dispenser !</h1>
+          </div>
+        </ul>
+      )}
     </nav>
   );
 }
