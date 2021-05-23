08b65e870769fa005b77201ba4ce124162fcec6c
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-05-23 18:28:44 +0200

    hotfix

--- src/components/GetIngredientsCocktails.jsx
+++ src/components/GetIngredientsCocktails.jsx
@@ -46,7 +46,6 @@ const GetIngredientsCocktails = () => {
                 />
                 {cocktail.strDrink}
               </Link>
-              )
             </div>
           ))}
         </div>
