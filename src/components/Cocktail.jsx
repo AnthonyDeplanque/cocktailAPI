0aabfe9c5adf433d4483c30f460a890f0fa0b5c6
Anthony <anthony.deplanque3184@gmail.com>
2021-05-26 10:36:07 +0200

    HOTFIX // loading screen

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -44,105 +44,113 @@ export default function Cocktail({ ...props }) {
   return (
     <div className="card-block">
       <div className="cardDrink">
-        <div className="inner-cardDrink">
-          <h1>{strDrink}</h1>
-          {strDrinkAlternate && (
-            <p>
-              <em>{strDrinkAlternate}</em>
-            </p>
-          )}
-          <div className="cocktail-elements">
-            <div className="cocktail-intro">
-              <h3>{strAlcoholic}</h3>
-              <img className="cocktail-image" src={strDrinkThumb} alt={idDrink} />
+        {strDrink ? (
+          <div className="inner-cardDrink">
+            <h1>{strDrink}</h1>
+            {strDrinkAlternate && (
               <p>
-                serve in a <strong>{strGlass}</strong>
+                <em>{strDrinkAlternate}</em>
               </p>
-            </div>
-            <div className="cocktail-making">
-              <div className="ingredients-list">
-                <ul>
-                  {strIngredient && (
-                    <li className="ingredient">
-                      {strMeasure1} <strong>{strIngredient}</strong>
-                    </li>
-                  )}
-                  {strIngredient2 && (
-                    <li className="ingredient">
-                      {strMeasure2} <strong>{strIngredient2}</strong>
-                    </li>
-                  )}
-                  {strIngredient3 && (
-                    <li className="ingredient">
-                      {strMeasure3} <strong>{strIngredient3}</strong>
-                    </li>
-                  )}
-                  {strIngredient4 && (
-                    <li className="ingredient">
-                      {strMeasure4} <strong>{strIngredient4}</strong>
-                    </li>
-                  )}
-                  {strIngredient5 && (
-                    <li className="ingredient">
-                      {strMeasure5} <strong>{strIngredient5}</strong>
-                    </li>
-                  )}
-                  {strIngredient6 && (
-                    <li className="ingredient">
-                      {strMeasure6} <strong>{strIngredient6}</strong>
-                    </li>
-                  )}
-                  {strIngredient7 && (
-                    <li className="ingredient">
-                      {strMeasure7} <strong>{strIngredient7}</strong>
-                    </li>
-                  )}
-                  {strIngredient8 && (
-                    <li className="ingredient">
-                      {strMeasure8} <strong>{strIngredient8}</strong>
-                    </li>
-                  )}
-                  {strIngredient9 && (
-                    <li className="ingredient">
-                      {strMeasure9} <strong>{strIngredient9}</strong>
-                    </li>
-                  )}
-                  {strIngredient10 && (
-                    <li className="ingredient">
-                      {strMeasure10} <strong>{strIngredient10}</strong>
-                    </li>
-                  )}
-                  {strIngredient11 && (
-                    <li className="ingredient">
-                      {strMeasure11} <strong>{strIngredient11}</strong>
-                    </li>
-                  )}
-                  {strIngredient12 && (
-                    <li className="ingredient">
-                      {strMeasure12} <strong>{strIngredient12}</strong>
-                    </li>
-                  )}
-                  {strIngredient13 && (
-                    <li className="ingredient">
-                      {strMeasure13} <strong>{strIngredient13}</strong>
-                    </li>
-                  )}
-                  {strIngredient14 && (
-                    <li className="ingredient">
-                      {strMeasure14} <strong>{strIngredient14}</strong>
-                    </li>
-                  )}
-                  {strIngredient15 && (
-                    <li className="ingredient">
-                      {strMeasure15} <strong>{strIngredient15}</strong>
-                    </li>
-                  )}
-                </ul>
+            )}
+            <div className="cocktail-elements">
+              <div className="cocktail-intro">
+                <h3>{strAlcoholic}</h3>
+                <img
+                  className="cocktail-image"
+                  src={strDrinkThumb}
+                  alt={idDrink}
+                />
+                <p>
+                  serve in a <strong>{strGlass}</strong>
+                </p>
               </div>
-              {strInstructions && <p>{strInstructions}</p>}
+              <div className="cocktail-making">
+                <div className="ingredients-list">
+                  <ul>
+                    {strIngredient && (
+                      <li className="ingredient">
+                        {strMeasure1} <strong>{strIngredient}</strong>
+                      </li>
+                    )}
+                    {strIngredient2 && (
+                      <li className="ingredient">
+                        {strMeasure2} <strong>{strIngredient2}</strong>
+                      </li>
+                    )}
+                    {strIngredient3 && (
+                      <li className="ingredient">
+                        {strMeasure3} <strong>{strIngredient3}</strong>
+                      </li>
+                    )}
+                    {strIngredient4 && (
+                      <li className="ingredient">
+                        {strMeasure4} <strong>{strIngredient4}</strong>
+                      </li>
+                    )}
+                    {strIngredient5 && (
+                      <li className="ingredient">
+                        {strMeasure5} <strong>{strIngredient5}</strong>
+                      </li>
+                    )}
+                    {strIngredient6 && (
+                      <li className="ingredient">
+                        {strMeasure6} <strong>{strIngredient6}</strong>
+                      </li>
+                    )}
+                    {strIngredient7 && (
+                      <li className="ingredient">
+                        {strMeasure7} <strong>{strIngredient7}</strong>
+                      </li>
+                    )}
+                    {strIngredient8 && (
+                      <li className="ingredient">
+                        {strMeasure8} <strong>{strIngredient8}</strong>
+                      </li>
+                    )}
+                    {strIngredient9 && (
+                      <li className="ingredient">
+                        {strMeasure9} <strong>{strIngredient9}</strong>
+                      </li>
+                    )}
+                    {strIngredient10 && (
+                      <li className="ingredient">
+                        {strMeasure10} <strong>{strIngredient10}</strong>
+                      </li>
+                    )}
+                    {strIngredient11 && (
+                      <li className="ingredient">
+                        {strMeasure11} <strong>{strIngredient11}</strong>
+                      </li>
+                    )}
+                    {strIngredient12 && (
+                      <li className="ingredient">
+                        {strMeasure12} <strong>{strIngredient12}</strong>
+                      </li>
+                    )}
+                    {strIngredient13 && (
+                      <li className="ingredient">
+                        {strMeasure13} <strong>{strIngredient13}</strong>
+                      </li>
+                    )}
+                    {strIngredient14 && (
+                      <li className="ingredient">
+                        {strMeasure14} <strong>{strIngredient14}</strong>
+                      </li>
+                    )}
+                    {strIngredient15 && (
+                      <li className="ingredient">
+                        {strMeasure15} <strong>{strIngredient15}</strong>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+                {strInstructions && <p>{strInstructions}</p>}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <p>Now loading</p>
+        )}
       </div>
     </div>
   );
